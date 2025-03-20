5bde204673280097b327abd4fe160dd9535b48f9
TriTran <thanhtritran8@gmail.com>
2025-03-20 09:27:12 +0700

    Backend: set up for deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,6 +14,10 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health Ok!" });
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
 });
