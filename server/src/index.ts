19897d985d0305f5f638ab34bdf09d6c635e4d9a
TriTran <thanhtritran8@gmail.com>
2025-03-20 09:19:15 +0700

    Init project

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(7000, () => {
+  console.log("Server is running on port 7000");
+});
