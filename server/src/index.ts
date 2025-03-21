d93fe827720fe2b6046fb39d83974c7340926c91
TriTran <thanhtritran8@gmail.com>
2025-03-21 11:11:29 +0700

    SERVER: update cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://learnlink-beige.vercel.app/"],
+    origin: "https://learnlink-beige.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
