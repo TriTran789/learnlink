de5c78b230c4300fa706b4cb7c02f80d1396a99d
TriTran <thanhtritran8@gmail.com>
2025-03-21 11:06:27 +0700

    BACKEND: add origin to cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,11 +13,13 @@ mongoose
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://learnlink-beige.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.get("/health", async (req: Request, res: Response) => {
