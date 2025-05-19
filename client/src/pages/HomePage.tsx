927c92d94b251bece7e0b4e2b621629044eacf5a
TriTran <thanhtritran8@gmail.com>
2025-05-19 17:19:52 +0700

    add test button

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <main className="bg-black min-h-screen text-white">
       <Header />
+      <Link to="/test">test</Link>
     </main>
   );
 };
