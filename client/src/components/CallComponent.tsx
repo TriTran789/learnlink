ea77522067c674f367b219733d543957f4f977f4
TriTran <thanhtritran8@gmail.com>
2025-05-23 23:31:46 +0700

    CLIENT: test data

--- client/src/components/CallComponent.tsx
+++ client/src/components/CallComponent.tsx
@@ -58,8 +58,9 @@ const CallComponent = ({ data }: Props) => {
   const [client, setClient] = useState<StreamVideoClient>();
   const [call, setCall] = useState<Call>();
 
+  console.log(data)
+
   useEffect(() => {
-    console.log("apiKey", import.meta.env.VITE_STREAM_API_KEY);
     const client = new StreamVideoClient({
       apiKey: import.meta.env.VITE_STREAM_API_KEY,
       user: data.user,
